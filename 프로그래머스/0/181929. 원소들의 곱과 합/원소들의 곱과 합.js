68a4951476bb152a95c1e559ef05b82629b4337f
calelix <jgpark7281@gmail.com>
2026-02-08 20:18:00 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    const product = num_list.reduce(
+        (acc, cur) => acc * cur,
+        1
+    )
+
+    const sum = num_list.reduce(
+        (acc, cur) => acc + cur,
+        0
+    )
+
+    return product > sum * sum ? 0 : 1
+}
